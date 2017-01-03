368b4fa9f79bac172dfd2f3c8d0456721e2721cf
teirm <tribunus.vii@gmail.com>
2017-01-03 17:15:10 -0500

    Learning higher ordered functions

---
+++ ch_5/examples.hs
@@ -0,0 +1,23 @@
+multThree :: Int -> Int -> Int -> Int
+multThree x y z = x * y * z
+
+compareWithHundred :: Int -> Ordering
+compareWithHundred = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+
+-- f must take two arguments a , b 
+-- Means f can't be a function like (+3)
+-- since this only takes one argument
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys

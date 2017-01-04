3f9c2b8bf4767cf9156d7c4718bb195bd8063a9c
teirm <tribunus.vii@gmail.com>
2017-01-03 19:30:16 -0500

    Fun times with Higher Ordered Functions

--- ch_5/examples.hs
+++ ch_5/examples.hs
@@ -21,3 +21,81 @@ zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+-- Could let f = g where g x y = f y x
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f y x = f x y
+
+-- Functional Programmer's Toolbox Examples
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = 
+    let smallerOrEqual = filter (<= x) xs
+        larger = filter (> x) xs
+    in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
+largestDivisible :: Integer
+largestDivisible = head (filter p [99999,99998..])
+    where p x = x `mod` 3829 == 0
+
+-- Collatz Chain
+-- GHCI does not like evaluating 10 ^ (10 ^ 10)
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+    | even n = n:chain (n `div` 2)
+    | odd n  = n:chain (n*3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
+
+-- numLongChains with a lambda
+numLongChains' :: Int
+numLongChains' = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+-- flip with lambdas
+my_flip :: (a -> b -> c) -> b -> a -> c
+my_flip f = \x y -> f y x 
+
+-- Sum with left fold
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
+
+-- Map with right fold
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = foldr (\x acc -> f x : acc) [] xs
+
+-- Elem with right fold
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
+
+-- Max with foldl1
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldl1 max
+
+-- Some fold examples
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
+
+reverse'' :: [a] -> [a]
+reverse'' = foldl (flip (:)) []
+
+product' :: (Num a) => [a] -> a
+product' = foldl (*) 1
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+and' :: [Bool] -> Bool
+and' xs = foldr (&&) True xs
+
+sqrtSums :: Int
+sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
+
+-- Point free style and composition
+oddSquareSum :: Integer
+oddSquareSum = sum . takeWhile (<10000) . filter odd $ map (^2) [1..]

ebeb05715cfbcd4d171a97c20e5a4ae726890770
teirm <tribunus.vii@gmail.com>
2017-01-07 15:26:21 -0500

    Using some new modules

---
+++ ch_6/examples.hs
@@ -0,0 +1,25 @@
+import Data.List 
+import Data.Char
+
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . nub
+
+wordNums :: String -> [(String, Int)]
+wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
+
+isIn :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
+
+-- Caesar Cipher
+encode :: Int -> String -> String
+-- As a function composition map (chr . (+ offset) . ord)
+encode offset msg = map (\c -> chr $ ord c + offset) msg
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
+
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
+
+firstTo :: Int -> Maybe Int
+firstTo n = find (\x -> digitSum x == n) [1..]

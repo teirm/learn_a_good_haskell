15fe100c03448b551e5c7c936664b5de4db0618b
teirm <tribunus.vii@gmail.com>
2017-01-11 21:43:32 -0500

    Record Syntax in haskell

---
+++ ch_7/person.hs
@@ -0,0 +1,10 @@
+-- Defining data types using record syntax
+
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String } deriving (Show)
+
+

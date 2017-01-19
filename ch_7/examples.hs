29ef9b5b2839a46d656d09cdeb6e9f46d4d09eef
teirm <tribunus.vii@gmail.com>
2017-01-18 20:01:59 -0500

    Recursive Data Structures

--- ch_7/examples.hs
+++ ch_7/examples.hs
@@ -66,4 +66,18 @@ phoneBook =
     ,("penny", "853-2429")
     ]
 
+-- Recursive Data Structures
+-- left associative fixity 5 :-:
+-- Number is tightness of binding
+infixr 5 -++
+(-++) :: [a] -> [a] -> [a]
+[]      -++ ys = ys
+(x:xs)  -++ ys = x : (xs -++ ys)
 
+infixr 5 ^++
+(^++) :: List a -> List a -> List a
+Empty ^++ ys = ys
+(x :-: xs) ^++ ys = x :-: (xs ^++ ys)
+
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)

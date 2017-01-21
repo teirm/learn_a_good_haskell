c619c22f0951d084e91833235fdae9ba65333a89
teirm <tribunus.vii@gmail.com>
2017-01-21 14:03:27 -0500

    Functors

--- ch_7/type_classes.hs
+++ ch_7/type_classes.hs
@@ -10,3 +10,35 @@ instance Show TrafficLight where
     show Red = "Red Light"
     show Yellow = "Yellow Light"
     show Green = "Green Light"
+
+-- A Yes No Type Class
+class YesNo a where
+    yesno :: a -> Bool
+
+-- Instances of the YesNo Class
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing = False
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _ = True
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult =
+    if yesno yesnoVal
+        then yesResult
+        else noResult
+
+

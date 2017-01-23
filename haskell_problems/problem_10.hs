53c63421805fcaab08f56f89799156536d923758
teirm <tribunus.vii@gmail.com>
2017-01-22 21:54:27 -0500

    Commented problem 10

--- haskell_problems/problem_10.hs
+++ haskell_problems/problem_10.hs
@@ -1,4 +1,7 @@
 import Data.List
+-- Technically cheating since I did not write my own
+-- group
+-- Will fix when possible
 
 encode :: (Eq a) => [a] -> [([a], Int)]
 encode = map (\word -> (word, length word)) . group 
